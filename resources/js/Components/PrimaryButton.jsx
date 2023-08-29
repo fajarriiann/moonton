ea031ecdc6e2043191e983692b53e85fc2292d1f
fajarrians <fajarrians578@gmail.com>
2023-08-29 14:30:29 +0700

    feat: slicing sign in and sign up

--- resources/js/Components/PrimaryButton.jsx
+++ resources/js/Components/PrimaryButton.jsx
@@ -1,13 +1,21 @@
-export default function PrimaryButton({ className = '', disabled, children, ...props }) {
+import PropTypes from "prop-types";
+
+PrimaryButton.propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node,
+    type: PropTypes.oneOf(["button", "submit", "reset"]),
+    variant: PropTypes.oneOf(["primary", "warning", "danger", "light-outline", "white-outline"]),
+    processing: PropTypes.bool,
+};
+
+export default function PrimaryButton({ className = '', children, type = "submit", variant = "primary", processing, ...props }) {
     return (
         <button
             {...props}
             className={
-                `inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ${
-                    disabled && 'opacity-25'
-                } ` + className
+                `rounded-2xl py-[13px] text-center w-full ${processing && 'opacity-30'} btn-${variant}` + className
             }
-            disabled={disabled}
+            disabled={processing}
         >
             {children}
         </button>
